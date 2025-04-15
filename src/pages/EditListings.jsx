540f3d341a40029e95c5e02424e7fae119a7c464
munal777 <munalpoudel3@gmail.com>
2025-04-15 22:39:08 +0545

    improved the functionality of edit component

--- src/pages/EditListings.jsx
+++ src/pages/EditListings.jsx
@@ -487,7 +487,7 @@ const EditListing = () => {
               <div className="flex justify-end space-x-3 pt-4">
                 <button
                   type="button"
-                  onClick={() => navigate(`/item/${id}`)}
+                  // onClick={() => navigate(`/item/${id}`)}  -commmented just for commit
                   className="px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   Cancel
